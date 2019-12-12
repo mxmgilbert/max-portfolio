3ec991b13368729fbfb101ba426e0a18fc924db9
Maxime Gilbert <mxm.gilbert@gmail.com>
2019-12-12 18:31:23 +0100

    Add external link for projects

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -1,16 +1,25 @@
-import {format, distanceInWords, differenceInDays} from 'date-fns'
-import React from 'react'
-import {Link} from 'gatsby'
-import {buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
-import BlockContent from './block-content'
-import Container from './container'
-import RoleList from './role-list'
+import { format, distanceInWords, differenceInDays } from "date-fns";
+import React from "react";
+import { Link } from "gatsby";
+import { buildImageObj } from "../lib/helpers";
+import { imageUrlFor } from "../lib/image-url";
+import BlockContent from "./block-content";
+import Container from "./container";
+import RoleList from "./role-list";
 
-import styles from './project.module.css'
+import styles from "./project.module.css";
 
-function Project (props) {
-  const {_rawBody, title, categories, mainImage, members, publishedAt, relatedProjects} = props
+function Project(props) {
+  const {
+    _rawBody,
+    title,
+    externalLink,
+    categories,
+    mainImage,
+    members,
+    publishedAt,
+    relatedProjects
+  } = props;
   return (
     <article className={styles.root}>
       {props.mainImage && mainImage.asset && (
@@ -19,7 +28,7 @@ function Project (props) {
             src={imageUrlFor(buildImageObj(mainImage))
               .width(1200)
               .height(Math.floor((9 / 16) * 1200))
-              .fit('crop')
+              .fit("crop")
               .url()}
             alt={mainImage.alt}
           />
@@ -30,16 +39,19 @@ function Project (props) {
           <div className={styles.mainContent}>
             <h1 className={styles.title}>{title}</h1>
             {_rawBody && <BlockContent blocks={_rawBody || []} />}
+            <a href={externalLink} target="_blank">
+              Découvir le site web
+            </a>
           </div>
           <aside className={styles.metaContent}>
             {publishedAt && (
               <div className={styles.publishedAt}>
                 {differenceInDays(new Date(publishedAt), new Date()) > 3
                   ? distanceInWords(new Date(publishedAt), new Date())
-                  : format(new Date(publishedAt), 'MMMM Do YYYY')}
+                  : format(new Date(publishedAt), "MMMM Do YYYY")}
               </div>
             )}
-            {members && members.length > 0 && <RoleList items={members} title='Project members' />}
+            {members && members.length > 0 && <RoleList items={members} title="Project members" />}
             {categories && categories.length > 0 && (
               <div className={styles.categories}>
                 <h3 className={styles.categoriesHeadline}>Categories</h3>
@@ -70,7 +82,7 @@ function Project (props) {
         </div>
       </Container>
     </article>
-  )
+  );
 }
 
-export default Project
+export default Project;
